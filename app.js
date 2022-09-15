813abe88928d2cf319cf96fe095eba2688673d37
Anees Ur Rehman <anees.rehman@daoproptech.com>
2022-09-15 23:41:20 +0500

    Using Express Router

--- app.js
+++ app.js
@@ -4,19 +4,13 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
+//Importing files from routes
+const adminRoute = require('./routes/admin');
+const shopRoute = require('./routes/shop');
+
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use('/add-products', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title" /><button type="submit">Add Product</button></form>')
-})
-
-app.use('/product', (req,res,next)=> {
-    console.log(req.body);
-    res.redirect('/')
-})
-
-app.use('/',(req, res, next) => {
-    res.send('<h1>Hello from express..</h1>')
-})
+app.use(adminRoute);
+app.use(shopRoute);
 
 app.listen(3000);
