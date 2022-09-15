71701ab95d80252f672ece3ada30271902e70124
Anees Ur Rehman <anees.rehman@daoproptech.com>
2022-09-15 23:47:42 +0500

    Filtering Rout Path by adding prefix /admin/--our-route--

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
 
-app.use(adminRoute);
+app.use('/admin',adminRoute);
 app.use(shopRoute);
 
 app.use((req,res,next)=> {
