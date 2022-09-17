b5a3cb8633dee08b7a316aa76fdafeb6b4806d0f
Anees Ur Rehman <anees.rehman@daoproptech.com>
2022-09-17 16:38:28 +0500

    Sharing data accross request users

--- app.js
+++ app.js
@@ -6,17 +6,17 @@ const path = require('path')
 const app = express();
 
 //Importing files from routes
-const adminRoute = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoute = require('./routes/shop');
 
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use('/admin',adminRoute);
+app.use('/admin',adminData.routes);
 app.use(shopRoute);
 
 app.use((req,res,next)=> {
-    res.status(404).sendFile(path.join(__dirname, './', 'views', 'page-not-found.html'))
+    res.status(404).sendFile(path.join(__dirname, './', 'views', '404.html'))
 })
 
 
