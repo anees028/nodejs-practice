cdef9537b2dd42fc27e655b0643d8cef45bcc795
Anees Ur Rehman <anees.rehman@daoproptech.com>
2022-10-07 12:29:14 +0500

    Sequelize => Connecting DB using sequelize npm package

--- utils/database.js
+++ utils/database.js
@@ -1,14 +1,9 @@
-const mysql = require('mysql2');
+const Sequelize = require('sequelize');
 
-const pool = mysql.createPool({
-    host:'localhost',
-    user:'root',
-    port:'3306',
-    database:'node-complete',
-    password:'admin',
-})
+const sequelize = new Sequelize('node-complete', 'root', 'admin', {
+    dialect: 'mysql', 
+    host:'localhost'
+});
 
 
-module.exports = pool.promise();
-
-
+module.exports = sequelize;
