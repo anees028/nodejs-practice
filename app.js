a27bc9dcf81bc911285ba2b8361b8e3e5335f6ba
Anees Ur Rehman <anees.rehman@daoproptech.com>
2022-11-07 00:35:19 +0500

    MongoDB relation between products and user established. by using referrence id.

--- app.js
+++ app.js
@@ -13,12 +13,18 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const User = require('./models/user');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use((req, res, next) => {
-  next();
+  User.findById("636803763b605f67b432729e").then(user => {
+    req.user = user;
+    next();
+  }).catch(err => {
+    console.log(err);
+  })
 });
 
 app.use('/admin', adminRoutes);
--- models/user.js
+++ models/user.js
@@ -1,23 +1,38 @@
-const Sequelize = require('sequelize');
-const sequelize = require('../utils/database');
+const getDb = require('../utils/mongo-database').getDb;
+const mongodb = require('mongodb');
 
-//Defining the model for database
-const User = sequelize.define('user',{
-     id:{
-        type: Sequelize.INTEGER,
-        autoIncrement: true,
-        allowNull: false,
-        primaryKey: true,
-    },
-    name:{
-        type:Sequelize.STRING,
-        allowNull:false
-    },
-    email:{
-        type: Sequelize.STRING,
-        allowNull:false,
+class User {
+    constructor(username,email, id){
+        this.username = username;
+        this.email = email;
+        this._id = id ? new mongodb.ObjectId(id) : null;
     }
-});
+
+    save(){
+        const db = getDb();
+        let dbOp;
+        if(this._id){
+
+        }
+        else{
+            dbOp = db.collection('users').insertOne(this)
+        }
+        return dpOp.then(user => {
+            console.log(user);
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+
+    static findById(userId){
+        const db = getDb();
+        return db.collection('users').find({_id: new mongodb.ObjectId(userId)}).next().then(user => {
+            return user;
+        }).catch(err => {
+            console.log(err);
+        })
+    }
+}
 
 module.exports = User;
 
